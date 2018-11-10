7a6c3e7d1b63058ef13b62d1283928c98b46dd54
LelouchLamperougeVI <33172704+LelouchLamperougeVI@users.noreply.github.com>
2018-11-10 13:38:56 -0700

    multithreaded support

--- MI_compile.m
+++ MI_compile.m
@@ -1,1 +1,1 @@
-mex -I./boost_1_68_0/ mat_knnSearch.cpp MI.cpp knnSearch.cpp
+mex CXXFLAGS='$CXXFLAGS -std=c++11 -pthread' -I./boost_1_68_0/ mat_knnSearch.cpp MI.cpp knnSearch.cpp
--- mat_knnSearch.cpp
+++ mat_knnSearch.cpp
@@ -3,7 +3,30 @@
 #include <matrix.h>
 #include "knnSearch.h"
 #include "MI.h"
+#include <thread>
 
+#define NUM_THREADS 8 //tweek number of threads for best performance for distinct systems
+
+int M, N, *index, *count, *ret, accum = 0;
+double *deconv, *sorted, *A, *I;
+
+void opRow(int i){
+        int j, k;
+        for(j = i + 1; j < N; j++) {
+                for(k = 0; k < count[i]; k++)
+                        A[k] = deconv[M*j + index[i*M + k]];
+                knnSearch(sorted+(i*M), index+(i*M), A, count[i], ret);
+
+                for(k = 0; k < count[j]; k++)
+                        A[k] = deconv[M*i + index[j*M + k]];
+                knnSearch(sorted+(j*M), index+(j*M), A, count[j], ret+M);
+
+                I[accum] = MI(ret, M, count[i], count[j]);
+                accum++;
+                for(k = 0; k < count[i]; k++) ret[index[i*M + k]] = 0.0; //reset array elements
+                for(k = 0; k < count[j]; k++) ret[M + index[j*M + k]] = 0.0;
+        }
+}
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         if(nrhs < 1)
@@ -11,24 +34,24 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         if(nrhs > 1)
                 mexWarnMsgTxt("Ignoring extra input arguments");
 
-        double *deconv = mxGetPr(prhs[0]);
-        int M = mxGetM(prhs[0]);
-        int N = mxGetN(prhs[0]);
+        deconv = mxGetPr(prhs[0]);
+        M = mxGetM(prhs[0]);
+        N = mxGetN(prhs[0]);
 
         if(M < 2)
                 mexErrMsgTxt("Recording has less than 2 sample frames");
         if(N < 2)
                 mexErrMsgTxt("At least 2 neurons are required to make comparison");
 
-        int i, j, k, accum = 0;
+        int i, j, k;
 
-        int *index = (int *) malloc(M*N * sizeof(int));
-        int *count = (int *) malloc(N * sizeof(int));
-        double *sorted = (double *) malloc(M*N * sizeof(double));
+        index = (int *) malloc(M*N * sizeof(int));
+        count = (int *) malloc(N * sizeof(int));
+        sorted = (double *) malloc(M*N * sizeof(double));
         struct blockHead *block = (struct blockHead*) malloc(M * sizeof(struct blockHead));
 
-        double *A = (double *) malloc(M * sizeof(double));
-        int *ret = (int *) calloc(M * 2, sizeof(int));
+        A = (double *) malloc(M * sizeof(double));
+        ret = (int *) calloc(M * 2, sizeof(int));
 
         for(i = 0; i < N; i++) {
                 for(j = 0; j < M; j++) {
@@ -48,24 +71,24 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 
         // int dims[2] = {M, N*(N-1)/2};
         plhs[0] = mxCreateDoubleMatrix(1, N*(N-1)/2, mxREAL);
-        double *I = mxGetPr(plhs[0]);
+        I = mxGetPr(plhs[0]);
 
-        for(i = 0; i < N; i++) {
-                for(j = i + 1; j < N; j++) {
-                        for(k = 0; k < count[i]; k++)
-                                A[k] = deconv[M*j + index[i*M + k]];
-                        knnSearch(sorted+(i*M), index+(i*M), A, count[i], ret);
+        std::thread t[NUM_THREADS];
+        int *last_elem = (int *) malloc(NUM_THREADS * sizeof(int));
+        for(i = 0; i < NUM_THREADS; i++) {
+                t[i] = std::thread(opRow, i);
+                t[i].detach();
+                last_elem[i] = (i + 1)*M - i - 1;
+        }
+        int next_row = NUM_THREADS;
 
-                        for(k = 0; k < count[j]; k++)
-                                A[k] = deconv[M*i + index[j*M + k]];
-                        knnSearch(sorted+(j*M), index+(j*M), A, count[j], ret+M);
+        while(next_row < N){
 
-                        I[accum] = MI(ret, M, count[i], count[j]);
-                        accum++;
-                        for(k = 0; k < count[i]; k++) ret[index[i*M + k]] = 0.0; //reset array elements
-                        for(k = 0; k < count[j]; k++) ret[M + index[j*M + k]] = 0.0;
-                }
         }
 
-        free(A); free(ret); free(index); free(count); free(sorted);
+        // for(i = 0; i < N; i++) {
+        //         opRow(i);
+        // }
+
+        free(A); free(ret); free(index); free(count); free(sorted); free(last_elem);
 }
